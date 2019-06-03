f4ac43f9d3f81de72b0430b18a1e428ee9e3b367
Risabh shao <rksha20@gmail.com>
2019-06-03 10:33:48 +0530

    03-06-2019

--- wampServer/www/MyProject/Signup.php
+++ wampServer/www/MyProject/Signup.php
@@ -1,1 +1,26 @@
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-4">
+                    <h1>Enter your Details</h1>
+        <form>
+            FirstName: <input type="text" class="form-control" name="firstname"><br><br>
+            Lastname: <input type="text" class="form-control" name="lastname"><br><br>
+            Emailid:  <input type="text" class="form-control" name="lastname"><br><br>
+            Password: <input type="text" class="form-control" name="password"><br><br>
+            <input type="button" value="Submit" class="btn btn-default">
+        </form>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+</html>
--- wampServer/www/MyProject/index.php
+++ wampServer/www/MyProject/index.php
@@ -8,19 +8,34 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link href="css/Style.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
     </head>
     <body>
-       <div class="header">
-           <div class="inner-header">
-               <div class="logo">LifeStyle</div>
-               <div class="header-link">
-              
-                   <a href="Signup.php"><span class="glyphicon glyphicon-user"></span>SignUp</a>
-               </div>
-               <div class="header-link">Login</div>
-           </div>
-       </div>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.php" class="navbar-brand">LifeStyle</a>
+                </div>
+                <div class="collapse navbar-collapse" id="MyNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="Signup.php"><span class="glyphicon glyphicon-user"></span>Signup</a></li>
+                        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"</span>Login</a></li>
+                     
+                    </ul>
+                    
+                </div>
+            </div>
+        </nav>
+            
+        </nav>
+         
         <div class="content">
             <div class="banner-image">
                 <div class="inner-banner-image">
